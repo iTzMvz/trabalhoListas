1816fdb6115b93c73b110cb9b2ac2f0a9ecab2d5
iTzMvz <matheus.vidal.zenere@gmail.com>
2023-05-02 11:52:27 -0300

    Lista de produtos

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+
+const listaProdutos = ref([
+    {
+        id: 1,
+        name: 'Item 1',
+        preco: 0,
+        quantidade: 0
+    },
+    {
+        id: 2,
+        name: 'Item 2',
+        preco: 0,
+        quantidade: 0
+    },
+    {
+        id: 3,
+        name: 'Item 3',
+        preco: 0,
+        quantidade: 0
+    }
+])
+</script>
+<template>
+    <ul>
+        <li v-for="(produto, id) in listaProdutos" :key="produto.id">{{ produto.name }} {{ produto.quantidade }}<button @click="produto.quantidade++">+</button> <button @click="produto.quantidade--">-</button></li>
+    </ul>
+</template>
