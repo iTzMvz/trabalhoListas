e01a221a37485616520ea53a7f02335cc479a1c7
iTzMvz <matheus.vidal.zenere@gmail.com>
2023-05-05 15:16:20 -0300

    carrinho

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,75 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
 
+const carrinho = ref([])
 const listaProdutos = ref([
-    {
-        id: 1,
-        name: 'Item 1',
-        preco: 0,
-        quantidade: 0
-    },
-    {
-        id: 2,
-        name: 'Item 2',
-        preco: 0,
-        quantidade: 0
-    },
-    {
-        id: 3,
-        name: 'Item 3',
-        preco: 0,
-        quantidade: 0
-    }
+  {
+    id: 1,
+    name: 'Calça',
+    preco: 59.99,
+    quantidade: 0
+  },
+  {
+    id: 2,
+    name: 'Camiseta',
+    preco: 49.99,
+    quantidade: 0
+  },
+  {
+    id: 3,
+    name: 'Tênis',
+    preco: 89.99,
+    quantidade: 0
+  },
+  {
+    id: 4,
+    name: 'Bermuda',
+    preco: 39.99,
+    quantidade: 0
+  }
 ])
+
+function addCarrinho(produto) {
+  carrinho.value.push(produto.id, produto.name, produto.preco, produto.quantidade);
+  calculaValorTotal();
+}
+
+const valorTotal = ref(0)
+
+function calculaValorTotal() {
+    valorTotal.value = 0;
+    if (carrinho.value.length > 0) {
+        for (item of carrinho.value) {
+            valorTotal.value = valorTotal.value + (item.preco * item.quantidade);
+        }
+    }
+}
 </script>
 <template>
-    <ul>
-        <li v-for="(produto, id) in listaProdutos" :key="produto.id">{{ produto.name }} {{ produto.quantidade }}<button @click="produto.quantidade++">+</button> <button @click="produto.quantidade--">-</button></li>
-    </ul>
+  <ul>
+    <li v-for="(produto, id) in listaProdutos" :key="produto.id">
+      <div>{{ produto.name }}</div>
+      <div>
+        {{ produto.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) }}
+      </div>
+      <div>Quantidade: {{ produto.quantidade }}</div>
+      <button @click="addCarrinho(produto)">Adicionar</button>
+      <button @click="produto.quantidade++" style="background-color: rgb(161, 211, 231)">+</button>
+      <button
+        v-if="produto.quantidade > 0"
+        @click="produto.quantidade--"
+        style="background-color: rgb(230, 43, 32)"
+      >
+        -
+      </button>
+    </li>
+  </ul>
 </template>
+<style scoped>
+button {
+  margin: 5px;
+  border-radius: 10px;
+  font-weight: bold;
+  background-color: rgb(231, 213, 130);
+}
+</style>
