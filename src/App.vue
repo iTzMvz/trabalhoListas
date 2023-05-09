63c14096242f410a19484b293f6b19a1f09da370
iTzMvz <matheus.vidal.zenere@gmail.com>
2023-05-09 14:45:57 -0300

    css produtos

--- src/App.vue
+++ src/App.vue
@@ -27,12 +27,24 @@ const listaProdutos = ref([
     nome: 'Bermuda',
     preco: 39.99,
     quantidade: 0
-  }
+  },
+  {
+    id: 5,
+    nome: 'Casaco',
+    preco: 99.99,
+    quantidade: 0
+  },
+  {
+    id: 6,
+    nome: '',
+    preco: 39.99,
+    quantidade: 0
+  },
 ])
 
 function addCarrinho(produto) {
   if (produto.quantidade == 0) {
-   alert(erroQnt.value)
+    alert(erroQnt.value)
   } else {
     carrinho.value.push(produto.id, produto.nome, produto.preco, produto.quantidade)
     calculaValorTotal()
@@ -53,19 +65,20 @@ function calculaValorTotal() {
 </script>
 <template>
   <ul>
-    <li v-for="(produto, i) in listaProdutos" :key="produto.id">
-      <div>{{ produto.nome }}</div>
-      <div>
-        {{ produto.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) }}
-      </div>
-      <div>Quantidade: {{ produto.quantidade }}</div>
-      <button @click="addCarrinho(produto)">Adicionar</button>
-      <button @click="produto.quantidade++" style="background-color: rgb(161, 211, 231)">+</button>
-      <button v-if="produto.quantidade > 0" @click="produto.quantidade--" style="background-color: rgb(230, 43, 32)">
-        -
-      </button>
-      <div></div>
-    </li>
+    <div class="produtos">
+      <li v-for="(produto, i) in listaProdutos" :key="produto.id">
+        <div>{{ produto.nome }}</div>
+        <div>
+          {{ produto.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) }}
+        </div>
+        <div>Quantidade: {{ produto.quantidade }}</div>
+        <button @click="addCarrinho(produto)">Adicionar</button>
+        <button @click="produto.quantidade++" style="background-color: rgb(161, 211, 231)">+</button>
+        <button v-if="produto.quantidade > 0" @click="produto.quantidade--" style="background-color: rgb(230, 43, 32)">
+          -
+        </button>
+      </li>
+    </div>
   </ul>
   <div>{{ carrinho }}</div>
 </template>
@@ -75,5 +88,14 @@ button {
   border-radius: 10px;
   font-weight: bold;
   background-color: rgb(231, 213, 130);
+}
+
+.produtos{
+display: flex;
+flex-wrap: wrap;
+flex-direction: row;
+align-content: center;
+justify-content: space-around;
+margin-right: 1000px;
 }
 </style>
