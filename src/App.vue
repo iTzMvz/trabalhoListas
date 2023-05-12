1d4de068d0ab32f2f6f42d7ca6f109eea724f21a
iTzMvz <matheus.vidal.zenere@gmail.com>
2023-05-12 15:07:28 -0300

    funcition de mostrar/esconder carrinho

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 
+const verCarrinho = ref(false)
 const erroQnt = ref('Adicione o produto primeiro antes de colocar no carrinho')
 const carrinho = ref([])
 const listaProdutos = ref([
@@ -57,9 +58,7 @@ const listaProdutos = ref([
     nome: 'Sapato',
     preco: 69.99,
     quantidade: 0
-  },
-
-
+  }
 ])
 
 function addCarrinho(id, nome, quantidade, preco) {
@@ -81,38 +80,61 @@ function calculaValorTotal() {
     }
   }
 }
+function limpaCarrinho() {
+  if (carrinho.value.length > 0) {
+    carrinho.value = []
+    valorTotal.value = 0
+  } else alert('O carrinho já está vazio')
+}
 
+function quantItens() {
+  if(carrinho[carrinho.findIndex(produto => produto.id == -1)]){
+    carrinho.push()
+  }
+}
 </script>
 <template>
   <ul>
     <div class="produtos">
       <li v-for="(produto, i) in listaProdutos" :key="i">
-        <div><img :src="produto.foto" alt=""></div>
         <div>{{ produto.nome }}</div>
         <div>
           {{ produto.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) }}
         </div>
         <div>Quantidade: {{ produto.quantidade }}</div>
-        <button
-          @click="addCarrinho(produto.id, produto.nome, produto.quantidade, produto.preco), produto.quantidade = 0">Adicionar</button>
-        <div><button @click="produto.quantidade++" id="mais">+</button>
-          <button v-if="produto.quantidade > 0" @click="produto.quantidade--" id="menos">
-            -
-          </button>
+        <button @click="addCarrinho(produto.id, produto.nome, produto.quantidade, produto.preco), produto.quantidade = 0">
+          Adicionar
+        </button>
+        <div>
+          <button @click="produto.quantidade++" id="mais">+</button>
+          <button v-if="produto.quantidade > 0" @click="produto.quantidade--" id="menos">-</button>
         </div>
       </li>
     </div>
   </ul>
   <ul>
-    <div class="carrinho">
-      <div v-if="valorTotal > 0">Valor total: {{ valorTotal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) }}</div> 
-      <li v-for="(produto, i) in carrinho" :key="i">
-        <div>{{ produto.nome }}</div>
-        <div>
-          {{ produto.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) }}
+    <button @click="verCarrinho = !verCarrinho" style="margin-left: 20px" v-if="carrinho.length > 0">
+      Ver carrinho
+    </button>
+    <div v-if="verCarrinho">
+      <div>
+        <button @click="limpaCarrinho()" style="margin-left: 20px" v-if="carrinho.length > 0">
+          Limpar carrinho
+        </button>
+      </div>
+      <div class="carrinho">
+        <div v-if="valorTotal > 0">
+          Valor total:
+          {{ valorTotal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) }}
         </div>
-        <div>Quantidade: {{ produto.quantidade }}</div>
-      </li>
+        <li v-for="(produto, i) in carrinho" :key="i">
+          <div>{{ produto.nome }}</div>
+          <div>
+            {{ produto.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) }}
+          </div>
+          <div>Quantidade: {{ produto.quantidade }}</div>
+        </li>
+      </div>
     </div>
   </ul>
 </template>
@@ -124,25 +146,31 @@ button {
   background-color: rgb(231, 213, 130);
   box-shadow: 3px 4px 5px rgb(133, 117, 45);
 }
+
 .produtos {
   display: flex;
   flex-wrap: wrap;
   flex-direction: row;
   justify-content: start;
-  }
+}
+
 .carrinho {
   display: flex;
   flex-direction: column;
 }
-li{
+
+li {
   margin-right: 20px;
   margin-left: 20px;
+  list-style: none;
 }
-#mais{  
+
+#mais {
   background-color: rgb(161, 211, 231);
   box-shadow: 2px 3px 5px rgb(54, 80, 90);
 }
-#menos{
+
+#menos {
   padding-right: 6px;
   padding-left: 6px;
   background-color: rgb(230, 43, 32);
